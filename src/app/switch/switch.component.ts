414bff2fd995a67982cb53d6c6a449060a44659b
Nikolai Pribitkov <pribitkov2013@mail.ru>
2020-08-27 11:36:16 +0300

    ngModel

---
+++ src/app/switch/switch.component.ts
@@ -0,0 +1,34 @@
+import { Component, forwardRef, Provider } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SwitchComponent),
+  multi: true
+};
+
+@Component({
+  selector: 'app-switch',
+  templateUrl: './switch.component.html',
+  styleUrls: ['./switch.component.scss'],
+  providers: [VALUE_ACCESSOR]
+})
+export class SwitchComponent implements ControlValueAccessor {
+  state = 'off';
+
+  onChange = (value: any) => { };
+
+  setState(state: string): void {
+    this.state = state;
+    this.onChange(this.state);
+  }
+
+  writeValue(state: string): void {
+    this.state = state;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void { }
+  setDisabledState?(isDisabled: boolean): void { }
+}
